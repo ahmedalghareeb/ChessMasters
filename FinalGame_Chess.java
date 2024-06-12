82368ee29375b3402b4b222e55de5ebe6d471b2e
Ahmed Ghareeb <ahmed.ghareeb@soti.net>
2024-06-11 20:52:17 -0400

    movement for knight and rook done

--- FinalGame_Chess.java
+++ FinalGame_Chess.java
@@ -23,210 +23,210 @@ public class FinalGame_Chess extends Applet implements ActionListener
     int col = 8;
     JButton a[] = new JButton [row * col];
     char piece[] [] = {
-        {'r', 'n', 'b', 'k', 'q', 'b', 'n', 'r'},
-        {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-        {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
-        {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
-        {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
-        {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
-        {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-        {'r', 'n', 'b', 'k', 'q', 'b', 'n', 'r'}};
+	{'r', 'n', 'b', 'k', 'q', 'b', 'n', 'r'},
+	{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+	{'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
+	{'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
+	{'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
+	{'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
+	{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+	{'r', 'n', 'b', 'k', 'q', 'b', 'n', 'r'}};
 
     char select[] [] = {
-        {'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u'},
-        {'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u'},
-        {'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u'},
-        {'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u'},
-        {'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u'},
-        {'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u'},
-        {'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u'},
-        {'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u'}};
+	{'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u'},
+	{'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u'},
+	{'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u'},
+	{'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u'},
+	{'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u'},
+	{'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u'},
+	{'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u'},
+	{'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u'}};
 
     char colour[] [] = {
-        {'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w'},
-        {'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w'},
-        {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
-        {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
-        {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
-        {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
-        {'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'},
-        {'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'}};
+	{'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w'},
+	{'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w'},
+	{'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
+	{'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
+	{'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
+	{'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
+	{'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'},
+	{'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'}};
 
     char bg[] [] = {
-        {'b', 'w', 'b', 'w', 'b', 'w', 'b', 'w'},
-        {'w', 'b', 'w', 'b', 'w', 'b', 'w', 'b'},
-        {'b', 'w', 'b', 'w', 'b', 'w', 'b', 'w'},
-        {'w', 'b', 'w', 'b', 'w', 'b', 'w', 'b'},
-        {'b', 'w', 'b', 'w', 'b', 'w', 'b', 'w'},
-        {'w', 'b', 'w', 'b', 'w', 'b', 'w', 'b'},
-        {'b', 'w', 'b', 'w', 'b', 'w', 'b', 'w'},
-        {'w', 'b', 'w', 'b', 'w', 'b', 'w', 'b'}};
+	{'b', 'w', 'b', 'w', 'b', 'w', 'b', 'w'},
+	{'w', 'b', 'w', 'b', 'w', 'b', 'w', 'b'},
+	{'b', 'w', 'b', 'w', 'b', 'w', 'b', 'w'},
+	{'w', 'b', 'w', 'b', 'w', 'b', 'w', 'b'},
+	{'b', 'w', 'b', 'w', 'b', 'w', 'b', 'w'},
+	{'w', 'b', 'w', 'b', 'w', 'b', 'w', 'b'},
+	{'b', 'w', 'b', 'w', 'b', 'w', 'b', 'w'},
+	{'w', 'b', 'w', 'b', 'w', 'b', 'w', 'b'}};
 
 
     public void init ()
     {
-        p_card = new Panel ();
-        p_card.setLayout (cdLayout);
-        // screen1 ();
-        // screen2 ();
-        screen3 ();
-        screen4 ();
-        screen5 ();
-        resize (630, 700);
-        setLayout (new BorderLayout ());
-        add ("Center", p_card);
+	p_card = new Panel ();
+	p_card.setLayout (cdLayout);
+	// screen1 ();
+	// screen2 ();
+	screen3 ();
+	screen4 ();
+	screen5 ();
+	resize (630, 700);
+	setLayout (new BorderLayout ());
+	add ("Center", p_card);
     }
 
 
     public void screen1 ()
     { //screen 1 is set up.
-        card1 = new Panel ();
-        card1.setBackground (new Color (255, 255, 255));
-        JLabel title = new JLabel ("Welcome");
-        JButton next = new JButton ("Next");
-        next.setActionCommand ("s2");
-        next.addActionListener (this);
-        card1.add (title);
-        card1.add (next);
-        p_card.add ("1", card1);
+	card1 = new Panel ();
+	card1.setBackground (new Color (255, 255, 255));
+	JLabel title = new JLabel ("Welcome");
+	JButton next = new JButton ("Next");
+	next.setActionCommand ("s2");
+	next.addActionListener (this);
+	card1.add (title);
+	card1.add (next);
+	p_card.add ("1", card1);
     }
 
 
     public void screen2 ()
     { //screen 2 is set up.
-        card2 = new Panel ();
-        card2.setBackground (new Color (255, 255, 255));
-        JLabel title = new JLabel ("Instructions");
-        JButton next = new JButton ("Next");
-        next.setActionCommand ("s3");
-        next.addActionListener (this);
-        card2.add (title);
-        card2.add (next);
-        p_card.add ("2", card2);
+	card2 = new Panel ();
+	card2.setBackground (new Color (255, 255, 255));
+	JLabel title = new JLabel ("Instructions");
+	JButton next = new JButton ("Next");
+	next.setActionCommand ("s3");
+	next.addActionListener (this);
+	card2.add (title);
+	card2.add (next);
+	p_card.add ("2", card2);
     }
 
 
     public void screen3 ()
     { //screen 3 is set up.
-        card3 = new Panel ();
-        card3.setBackground (new Color (255, 255, 255));
-        JLabel title = new JLabel ("Game");
+	card3 = new Panel ();
+	card3.setBackground (new Color (255, 255, 255));
+	JLabel title = new JLabel ("Game");
 
-        // turn
-        turnpic= new JLabel (createImageIcon("slytherinLogo.png"));
+	// turn
+	turnpic= new JLabel (createImageIcon("slytherinLogo.png"));
 
-        JButton next = new JButton ("Next");
-        next.setActionCommand ("s4");
-        next.addActionListener (this);
+	JButton next = new JButton ("Next");
+	next.setActionCommand ("s4");
+	next.addActionListener (this);
 
-        //Set up grid
-        Panel p = new Panel (new GridLayout (row, col));
-        int move = 0;
-        for (int i = 0 ; i < row ; i++)
-        {
-            for (int j = 0 ; j < col ; j++)
-            { 
+	//Set up grid
+	Panel p = new Panel (new GridLayout (row, col));
+	int move = 0;
+	for (int i = 0 ; i < row ; i++)
+	{
+	    for (int j = 0 ; j < col ; j++)
+	    { 
 				a [move] = new JButton(createImageIcon (piece [i] [j] + "" + colour [i] [j] + "" + bg [i] [j] + "" + select [i] [j] + ".jpg"));
 				a [move].setPreferredSize (new Dimension (75, 75));
 				a [move].addActionListener (this);
 				a [move].setActionCommand ("" + move);
 				p.add (a [move]);
 				move++;
-            }
-        }
-        card3.add (title);
-        card3.add (turnpic);
-        card3.add (p);
-        card3.add (next);
-        p_card.add ("3", card3);
+	    }
+	}
+	card3.add (title);
+	card3.add (turnpic);
+	card3.add (p);
+	card3.add (next);
+	p_card.add ("3", card3);
     }
 
     public void screen4 ()
     { //screen 4 is set up.
-        card4 = new Panel ();
-        card4.setBackground (new Color (255, 255, 255));
-        JLabel title = new JLabel ("You Win!");
-        JButton next = new JButton ("Next");
-        next.setActionCommand ("s5");
-        next.addActionListener (this);
-        card4.add (title);
-        card4.add (next);
-        p_card.add ("4", card4);
+	card4 = new Panel ();
+	card4.setBackground (new Color (255, 255, 255));
+	JLabel title = new JLabel ("You Win!");
+	JButton next = new JButton ("Next");
+	next.setActionCommand ("s5");
+	next.addActionListener (this);
+	card4.add (title);
+	card4.add (next);
+	p_card.add ("4", card4);
     }
 
 
     public void screen5 ()
     { //screen 5 is set up.
-        card5 = new Panel ();
-        card5.setBackground (new Color (255, 255, 255));
-        JLabel title = new JLabel ("You Lose.");
-        JButton next = new JButton ("Back to Introduction?");
-        next.setActionCommand ("s1");
-        next.addActionListener (this);
-        JButton end = new JButton ("Quit?");
-        end.setActionCommand ("s6");
-        end.addActionListener (this);
-        card5.add (title);
-        card5.add (next);
-        card5.add (end);
-        p_card.add ("5", card5);
+	card5 = new Panel ();
+	card5.setBackground (new Color (255, 255, 255));
+	JLabel title = new JLabel ("You Lose.");
+	JButton next = new JButton ("Back to Introduction?");
+	next.setActionCommand ("s1");
+	next.addActionListener (this);
+	JButton end = new JButton ("Quit?");
+	end.setActionCommand ("s6");
+	end.addActionListener (this);
+	card5.add (title);
+	card5.add (next);
+	card5.add (end);
+	p_card.add ("5", card5);
     }
 
 
     protected static ImageIcon createImageIcon (String path)
     { //change the red to your class name
-        java.net.URL imgURL = FinalGame_Chess.class.getResource (path);
-        if (imgURL != null)
-        {
-            return new ImageIcon (imgURL);
-        }
-        else
-        {
-            System.err.println ("Couldn't find file: " + path);
-            return null;
-        }
+	java.net.URL imgURL = FinalGame_Chess.class.getResource (path);
+	if (imgURL != null)
+	{
+	    return new ImageIcon (imgURL);
+	}
+	else
+	{
+	    System.err.println ("Couldn't find file: " + path);
+	    return null;
+	}
     }
 
 
     public void redraw ()
     {
-        int move = 0;
-        for (int i = 0 ; i < row ; i++)
-        {
-            for (int j = 0 ; j < col ; j++)
-            {
+	int move = 0;
+	for (int i = 0 ; i < row ; i++)
+	{
+	    for (int j = 0 ; j < col ; j++)
+	    {
 				a [move].setIcon (createImageIcon (piece [i] [j] + "" + colour [i] [j] + "" + bg [i] [j] + "" + select [i] [j] + ".jpg"));
 				move++;
-            }
-        }
+	    }
+	}
     }
 
     // Pieces Movement selection
     public void selectPawn(int x, int y)
     {
-        // Pawn Movement
-        if (colour[x][y] == 'b' && x == 6)
-        {
+	// Pawn Movement
+	if (colour[x][y] == 'b' && x == 6)
+	{
 			System.out.println("black and 6");
-            select[x - 1][y] = 's';
-            select[x - 2][y] = 's';
-        }
-        else if (colour[x][y] == 'w' && x == 1)
-        {
+	    select[x - 1][y] = 's';
+	    select[x - 2][y] = 's';
+	}
+	else if (colour[x][y] == 'w' && x == 1)
+	{
 			System.out.println("white and 1");
-            select[x + 1][y] = 's';
-            select[x + 2][y] = 's';
-        }
-        else if (colour[x][y] == 'b')
-        {
+	    select[x + 1][y] = 's';
+	    select[x + 2][y] = 's';
+	}
+	else if (colour[x][y] == 'b')
+	{
 			System.out.println("black");
-            select[x - 1][y] = 's';
-        }
-        else if (colour[x][y] == 'w')
-        {
+	    select[x - 1][y] = 's';
+	}
+	else if (colour[x][y] == 'w')
+	{
 			System.out.println("white");
-            select[x + 1][y] = 's';
-        }
+	    select[x + 1][y] = 's';
+	}
     }
 
 	public void selectKing(int x, int y)
@@ -264,7 +264,41 @@ public class FinalGame_Chess extends Applet implements ActionListener
 			select[x + 1][y] = 's';
 		}
 	}
-
+	public void selectKnight(int x, int y)
+	{
+		if (x - 2 >= 0 && y + 1 < col && colour[x - 2][y + 1] != turn)
+		{
+			select[x - 2][y + 1] = 's';
+		}
+		if (x - 2 >= 0 && y - 1 >= 0 && colour[x - 2][y - 1] != turn)
+		{
+			select[x - 2][y - 1] = 's';
+		}
+		if (x + 2 < row && y + 1 < col && colour[x + 2][y + 1] != turn)
+		{
+			select[x + 2][y + 1] = 's';
+		}
+		if (x + 2 < row && y - 1 >= 0 && colour[x + 2][y - 1] != turn)
+		{
+			select[x + 2][y - 1] = 's';
+		}
+		if (x - 1 >= 0 && y + 2 < col && colour[x - 1][y + 2] != turn)
+		{
+			select[x - 1][y + 2] = 's';
+		}
+		if (x - 1 >= 0 && y - 2 >= 0 && colour[x - 1][y - 2] != turn)
+		{
+			select[x - 1][y - 2] = 's';
+		}
+		if (x + 1 < row && y + 2 < col && colour[x + 1][y + 2] != turn)
+		{
+			select[x + 1][y + 2] = 's';
+		}
+		if (x + 1 < row && y - 2 >= 0 && colour[x + 1][y - 2] != turn)
+		{
+			select[x + 1][y - 2] = 's';
+		}
+	}
     public void actionPerformed (ActionEvent e)
     { //moves between the screens
 		if (e.getActionCommand ().equals ("s1"))
@@ -300,6 +334,12 @@ public class FinalGame_Chess extends Applet implements ActionListener
 				{
 					selectKing(x, y);
 				}
+				else if (piece[x][y] == 'n')
+				{
+					selectKnight(x, y);
+				}       
+					
+				
 				last = n;
 			}
 			else
